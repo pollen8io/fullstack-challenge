38773ec5cd85cd6ece155986e927a680882dcc50
Hayden Perry <haydencwperry@gmail.com>
2021-01-11 00:34:36 +0000

    Fixed eslint

---
+++ vendor/src/components/Document.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <textarea
+      :value="text"
+      @input="input"
+      @keyup="keyup"
+      @click="click"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Document',
+  components: {},
+  data: () => ({
+    text: ''
+  }),
+  methods: {
+    input (event) {
+      console.dir(event)
+    },
+    keyup (event) {
+      console.dir(event)
+    },
+    click (event) {
+      console.dir(event)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
